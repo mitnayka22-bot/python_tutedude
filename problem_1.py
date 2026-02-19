06db7a11758764572e9a1fccc0cf9037af34ed18
Mit Nayka <mitnayka22@gmail.com>
2026-02-19 12:53:35 +0530

    Add files via upload
    Assignment 1 in python course on Tutedude.

---
+++ problem_1.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+# addition
+sum = num1  + num2
+print(f"Addition : {sum}")
+
+# Subtraction
+diff = num1 - num2
+print("Subtraction: ",diff)
+
+# multiplication
+mulp = num1 * num2
+print("Multiplication : ",mulp)
+
+# Division
+
+div = num1 / num2
+print("Division : ",div)
